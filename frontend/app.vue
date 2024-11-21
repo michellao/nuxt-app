e7d92194fdbc5826251540abbc6e6cef24fd6949
Michel Lao <SansNom01@proton.me>
2024-11-21 15:35:39 +0100

    Remove layout

--- frontend/app.vue
+++ frontend/app.vue
@@ -1,11 +1,9 @@
 <template>
-  <NuxtLayout>
-    <v-app>
-      <div>
-        <NuxtPage/>
-      </div>
-    </v-app>
-  </NuxtLayout>
+  <v-app>
+    <div>
+      <NuxtPage/>
+    </div>
+  </v-app>
 </template>
 
 <style>
