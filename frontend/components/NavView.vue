69a3a49ff7a517f9d6d9728e494c1b9dfc8587c5
Michel Lao <SansNom01@proton.me>
2024-11-22 13:26:41 +0100

    Display the user name on nav

--- frontend/components/NavView.vue
+++ frontend/components/NavView.vue
@@ -1,18 +1,20 @@
 <script setup lang="ts">
 const cookies = extractCookieAuthenticate();
 const isDisconnect = useState(() => !isAuth(cookies));
+const userMe = await getUser();
 </script>
 
 <template>
     <div><h1>Managements Messages</h1></div>
     <div v-if="isDisconnect">
-            <a
-                href="https://discord.com/oauth2/authorize?client_id=1303701511888703509&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fapi%2Fauth-discord&scope=identify+messages.read"
-            >
-                <v-btn-secondary append-icon="mdi-login">Login with Discord</v-btn-secondary>
-            </a>
+        <a
+            href="https://discord.com/oauth2/authorize?client_id=1303701511888703509&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fapi%2Fauth-discord&scope=identify+messages.read"
+        >
+            <v-btn-secondary append-icon="mdi-login">Login with Discord</v-btn-secondary>
+        </a>
     </div>
     <div v-else>
+        {{ userMe?.global_name }}
         <a href="/api/logout-discord">
             <v-btn-secondary icon="mdi-logout"/>
         </a>
