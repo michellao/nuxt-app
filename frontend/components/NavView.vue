c3ae1dfe9eb650d0945aa01014358955d91c8811
Michel Lao <SansNom01@proton.me>
2024-11-25 16:42:41 +0100

    Add OAuth2 scope guilds

--- frontend/components/NavView.vue
+++ frontend/components/NavView.vue
@@ -8,7 +8,7 @@ const userMe = await getUser();
     <div><h1>Managements Messages</h1></div>
     <div v-if="isDisconnect">
         <a
-            href="https://discord.com/oauth2/authorize?client_id=1303701511888703509&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fapi%2Fauth-discord&scope=identify+messages.read"
+            href="https://discord.com/oauth2/authorize?client_id=1303701511888703509&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fapi%2Fauth-discord&scope=identify+messages.read+guilds"
         >
             <v-btn-secondary append-icon="mdi-login">Login with Discord</v-btn-secondary>
         </a>
