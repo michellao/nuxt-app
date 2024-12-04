d659c85f7e80a5e1ad3af32dfbe8378ca699b960
Michel Lao <SansNom01@proton.me>
2024-12-04 20:38:28 +0100

    Remove some security on verication authentication

--- frontend/utils/auth.ts
+++ frontend/utils/auth.ts
@@ -1,6 +1,5 @@
 import type { CookieType } from "./extract-cookies";
 
 export const isAuth = (cookiesAuth: CookieType) => {
-    return typeof cookiesAuth["access-token"] === 'string' && typeof cookiesAuth["refresh-token"] === 'string'
-        && typeof cookiesAuth["expires-in"] === 'string';
+    return typeof cookiesAuth["access-token"] === 'string' && typeof cookiesAuth["refresh-token"] === 'string';
 };
