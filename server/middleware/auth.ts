e7ed00a8adbf03ddee6a3e9bb9d85bfae21a3cbd
Michel Lao <SansNom01@proton.me>
2024-12-25 19:49:01 +0100

    Fix forwarding header to

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -2,8 +2,8 @@ export default defineEventHandler(async (event) => {
     const matcherPath = new RegExp("^/((?!_nuxt|__nuxt_.*|api/(auth|logout)-discord|api/users/@me))");
     if (event.path !== '/' && matcherPath.test(event.path)) {
         const cookies = extractCookieAuthenticateFromServer(event);
-        const isDisconnected = !(await isAuth(cookies));
-        if (isDisconnected) {
+        const isAuth = typeof cookies["access-token"] === 'string' && typeof cookies["refresh-token"] === 'string' && await event.$fetch('/api/users/@me') !== null;
+        if (!isAuth) {
             throw createError({
                 statusCode: 403,
             });
--- server/utils/authenticate.ts
+++
@@ -1,5 +0,0 @@
-import { type CookieType } from "~/utils/extract-cookies";
-
-export const isAuth = async (cookiesAuth: CookieType) => {
-    return typeof cookiesAuth["access-token"] === 'string' && typeof cookiesAuth["refresh-token"] === 'string' && await $fetch('/api/users/@me') !== null;
-};
