7dc29155726943d85ab5c0a9be58b34042a2d836
Michel Lao <SansNom01@proton.me>
2025-01-06 18:43:59 +0100

    Filter messages with the connected user

--- server/api/discord/channels/[channel]/messages.get.ts
+++ server/api/discord/channels/[channel]/messages.get.ts
@@ -1,8 +1,9 @@
 export default defineEventHandler(async (event) => {
     const channel = getRouterParam(event, 'channel');
+    const userMe = await event.$fetch('/api/users/@me');
     if (channel) {
         const messages = await getChannelMessages(channel);
-        return messages;
+        return messages.filter(m => m.author.id === userMe?.id);
     } else {
         throw createError({
             statusCode: 400
